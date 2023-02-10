dccfe85a4d3d1eb643d4c7c3a39c91f7d6a17234
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-02-10 17:06:08 +0530

    Update Hashset.java

--- src/main/java/pk/Hashset.java
+++ src/main/java/pk/Hashset.java
@@ -2,12 +2,12 @@ package pk;
 import java.util.*;
 import java.util.logging.Logger;
 
-class studentDetails {
+class Studentdetails {
     String studentName;
     String studentAddress;
     int id;
 
-    studentDetails(String studentName, String studentAddress, int id) {
+    Studentdetails(String studentName, String studentAddress, int id) {
         this.studentName = studentName;
         this.studentAddress = studentAddress;
         this.id = id;
@@ -23,22 +23,22 @@ public class Hashset{
     public static final Logger Log = Logger.getLogger("InfoLogging");
 
     public static void main(String[] args) {
-        HashSet<studentDetails> set = new HashSet<studentDetails>();
-        studentDetails sd1 = new studentDetails("prasanna", "1st street", 101);
+        HashSet<Studentdetails> set = new HashSet<Studentdetails>();
+        Studentdetails sd1 = new Studentdetails("prasanna", "1st street", 101);
         set.add(sd1);
-        sd1 = new studentDetails("nixon","2nd Street", 102);
+        sd1 = new Studentdetails("nixon","2nd Street", 102);
         set.add(sd1);
-        sd1 = new studentDetails("vignesh", "3rd Street", 103);
+        sd1 = new Studentdetails("vignesh", "3rd Street", 103);
         set.add(sd1);
         String s1;
 
-        for (studentDetails s : set) {
+        for (Studentdetails s : set) {
             s1 =s.studentName + " " + s.studentAddress + " " + s.id;
             Log.info(s1);
         }
         s1 = "" + set.remove(sd1);
         Log.info(s1);
-        for (studentDetails s : set) {
+        for (Studentdetails s : set) {
             s1 = s.studentName + " " + s.studentAddress + " " + s.id;
             Log.info(s1);
         }
