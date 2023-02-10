84ecf0907f363ed20f02985b76f67e11750f6335
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-02-10 16:50:41 +0530

    Update tree.java

--- src/main/java/pk/tree.java
+++ src/main/java/pk/tree.java
@@ -7,22 +7,22 @@ import java.util.Comparator;
 public class tree {
     public static final Logger Log = Logger.getLogger("InfoLogging");
   public static void main(String[] args) {
-    TreeSet<String> names = new TreeSet<>();
+    TreeSet<String> names = new TreeSet<>(new Comparator<String>() {
+        @Override
+        public int compare(String s1, String s2) {
+          return s1.compareTo(s2);
+        }
+      });
+
     names.add("prasanna");
     names.add("nixon");
     names.add("jeeva");
     names.add("jeshinth");
     names.add("thavasu");
+
     String tree ="TreeSet" + names;
     Log.info(tree);  
-    TreeSet<String> sortedNames = new TreeSet<>(new Comparator<String>() {
-      @Override
-      public int compare(String s1, String s2) {
-        return s1.compareTo(s2);
-      }
-    });
-    sortedNames.addAll(names);
-    String stree = "ascending order: " + sortedNames;
+    String stree = "ascending order: " + names;
     Log.info(stree);  
     stree = ""+names.contains("nixon");
     Log.info(stree);
